7eff41bc9e41edc2c2f37cad558f0ffadc576486
cynthiachinenye <cynthiachiagu@gmail.com>
2023-07-29 00:03:50 +0100

    new

---
+++ myapp/src/Datafetching.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react'
+import axios from'axios'
+
+function Datafetching() {
+    const [post,setPost] =
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Datafetching
