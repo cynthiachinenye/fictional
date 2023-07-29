f1ea1c64c05b2582e41426f1410633c47f65c408
cynthiachinenye <cynthiachiagu@gmail.com>
2023-07-29 03:16:53 +0100

    new

--- practice/src/Components/CounterTwo.js
+++ practice/src/Components/CounterTwo.js
@@ -1,7 +1,8 @@
 import React,{useReducer} from 'react'
 const initial = {
-    firstCounter:0
-    
+    firstCounter:0,
+    secondCounter:10
+
 }
 const reduce =(state,action)=>{
     switch(action.type){
@@ -9,6 +10,11 @@ const reduce =(state,action)=>{
             return {firstCounter:state.firstCounter + action.value}
             case 'decrement':
                 return {firstCounter:state.firstCounter - action.value}
+                case 'increment':
+                    return {secondCounter:state.secondCounter + action.value}
+                    case 'decrement':
+                        return {secondCounter:state.secondCounter - action.value}
+                       
                 case 'reset':
                     return initial
                     default:return state
@@ -23,6 +29,13 @@ function CounterTwo() {
       <button onClick={() => dispatch({type:'decrement',value:1})}>Decrement</button>
       <button onClick={()=> dispatch({type:'increment',value:5})}>Increment5</button>
       <button onClick={() => dispatch({type:'decrement',value:5})}>Decrement5</button>
+      <div>
+        <div>Count2 -{count.secondCounter}</div>
+        
+        <button onClick={()=> dispatch({type:'increment',value:10})}>Increment10</button>
+      <button onClick={() => dispatch({type:'decrement',value:10})}>Decrement10</button>
+      
+      </div>
       <button onClick ={() => dispatch({type:'reset'})}>Reest</button>
    
     </div>
