dc446ee59bac7d72df4cabe6985500cde2ecca7a
cynthiachinenye <cynthiachiagu@gmail.com>
2023-08-01 10:59:18 +0100

    update

--- renderdemo/src/App.js
+++ renderdemo/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
-import UseState from './Components/UseState/UseState';
+import UseReducer from './Components/UseReducer/UseReducer';
+// import UseState from './Components/UseState/UseState';
 
 function App() {
   return (
     <div className="App">
-      <UseState/>
+      {/* <UseState/> */}
+      <UseReducer/>
       </div>
   );
 }
